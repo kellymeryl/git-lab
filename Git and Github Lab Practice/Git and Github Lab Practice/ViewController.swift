03f7fd8ce75fa077a17796044f8dfe1ff6e21479
kellymeryl <owner@Owners-MacBook-Air.local>
2016-12-05 16:28:14 -0500

    initialized

---
+++ Git and Github Lab Practice/Git and Github Lab Practice/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Git and Github Lab Practice
+//
+//  Created by Kelly McNevin on 12/5/16.
+//  Copyright © 2016 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    class Car {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
